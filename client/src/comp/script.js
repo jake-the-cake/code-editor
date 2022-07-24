7cd0ebbfcf1882581e635ed638bb4ff2a1ff1463
jake-the-cake <askjake331@gmail.com>
2022-07-24 11:33:29 -0400

    output works

--- client/src/comp/script.js
+++ client/src/comp/script.js
@@ -1,2 +1,35 @@
 "use strict";
-console.log('js!');
+const out = document.getElementById('output-area');
+const loader = document.getElementById('output-loader');
+const update = document.getElementById('update-code');
+let isLoading = true;
+let isWorking = false;
+/*  ::: Loading widget  */
+//////////////////////////////////////////
+const toggleOutputLoader = () => {
+    loader === null || loader === void 0 ? void 0 : loader.classList.toggle('shrink');
+    setTimeout(() => loader === null || loader === void 0 ? void 0 : loader.classList.toggle('hide'), 100);
+    if (isLoading = true)
+        setTimeout(() => {
+            loader === null || loader === void 0 ? void 0 : loader.classList.toggle('none');
+        }, 400);
+    isLoading = false;
+};
+window.addEventListener('load', () => setTimeout(toggleOutputLoader, 800));
+/*  ::: Update Code Button  */
+//////////////////////////////////////////
+const handleUpdateCode = (event) => {
+    if (isWorking === false) {
+        event.preventDefault();
+        isWorking = true;
+        loader === null || loader === void 0 ? void 0 : loader.classList.toggle('none');
+        toggleOutputLoader();
+        isLoading = true;
+        const code = document.getElementById('input-area').value;
+        out.innerHTML = code;
+        isWorking = false;
+    }
+    else
+        console.log('first');
+};
+update === null || update === void 0 ? void 0 : update.addEventListener('click', handleUpdateCode);
--- client/src/raw/script.ts
+++ client/src/raw/script.ts
@@ -1,1 +1,36 @@
-console.log('js!')
+
+
+const out: HTMLElement | null = document.getElementById('output-area') as HTMLDivElement
+const loader = document.getElementById('output-loader')
+const update = document.getElementById('update-code')
+
+let isLoading = true
+let isWorking = false
+
+/*  ::: Loading widget  */
+//////////////////////////////////////////
+const toggleOutputLoader = () => {
+    loader?.classList.toggle('shrink')
+    setTimeout(() => loader?.classList.toggle('hide'), 100)
+    if (isLoading = true) setTimeout(() => {
+        loader?.classList.toggle('none')
+    }, 400)
+    isLoading = false
+}
+window.addEventListener('load', () => setTimeout(toggleOutputLoader,800))
+
+/*  ::: Update Code Button  */
+//////////////////////////////////////////
+const handleUpdateCode = (event: Event) => {
+    if (isWorking === false) {
+        event.preventDefault()
+        isWorking = true
+        loader?.classList.toggle('none')
+        toggleOutputLoader()
+        isLoading = true
+        const code = (document.getElementById('input-area') as HTMLTextAreaElement)!.value
+        out.innerHTML = code
+        isWorking = false
+    } else console.log('first')
+}
+update?.addEventListener('click', handleUpdateCode)
