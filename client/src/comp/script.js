52987d3530784754c7296b22f947a46f99f60575
jake-the-cake <askjake331@gmail.com>
2022-07-26 17:41:56 -0400

    resize and change buttons with sizing transition

--- client/src/comp/script.js
+++ client/src/comp/script.js
@@ -2,6 +2,12 @@
 const out = document.getElementById('output-area');
 const loader = document.getElementById('output-loader');
 const update = document.getElementById('update-code');
+const updateActive = document.getElementById('control-update');
+const editActive = document.getElementById('control-edit');
+const edit = document.getElementById('edit-code');
+const display = document.getElementById('display');
+const editor = document.getElementById('editor');
+const input = document.getElementById('input-area');
 let isLoading = true;
 let isWorking = false;
 /*  ::: Loading widget  */
@@ -22,14 +28,35 @@ const handleUpdateCode = (event) => {
     if (isWorking === false) {
         event.preventDefault();
         isWorking = true;
+        display.style.flex = '4';
+        editor.style.flex = '1';
+        editActive === null || editActive === void 0 ? void 0 : editActive.classList.toggle('none');
+        updateActive === null || updateActive === void 0 ? void 0 : updateActive.classList.toggle('none');
         loader === null || loader === void 0 ? void 0 : loader.classList.toggle('none');
         toggleOutputLoader();
-        isLoading = true;
         const code = document.getElementById('input-area').value;
         out.innerHTML = code;
+        isLoading = true;
         isWorking = false;
     }
     else
         console.log('first');
 };
 update === null || update === void 0 ? void 0 : update.addEventListener('click', handleUpdateCode);
+/*  ::: Edit Code Button  */
+//////////////////////////////////////////
+const handleEditCode = (event) => {
+    if (isWorking === false) {
+        isWorking = true;
+        event.preventDefault();
+        editActive === null || editActive === void 0 ? void 0 : editActive.classList.toggle('none');
+        updateActive === null || updateActive === void 0 ? void 0 : updateActive.classList.toggle('none');
+        display.style.flex = '1';
+        editor.style.flex = '4';
+        input === null || input === void 0 ? void 0 : input.focus();
+        isWorking = false;
+    }
+    else
+        console.log('second');
+};
+edit === null || edit === void 0 ? void 0 : edit.addEventListener('click', handleEditCode);
--- client/src/raw/script.ts
+++ client/src/raw/script.ts
@@ -3,6 +3,12 @@
 const out: HTMLElement | null = document.getElementById('output-area') as HTMLDivElement
 const loader = document.getElementById('output-loader')
 const update = document.getElementById('update-code')
+const updateActive = document.getElementById('control-update')
+const editActive = document.getElementById('control-edit')
+const edit = document.getElementById('edit-code')
+const display = document.getElementById('display')
+const editor = document.getElementById('editor')
+const input = document.getElementById('input-area')
 
 let isLoading = true
 let isWorking = false
@@ -25,12 +31,32 @@ const handleUpdateCode = (event: Event) => {
     if (isWorking === false) {
         event.preventDefault()
         isWorking = true
+        display!.style.flex = '4'
+        editor!.style.flex = '1'
+        editActive?.classList.toggle('none')
+        updateActive?.classList.toggle('none')
         loader?.classList.toggle('none')
         toggleOutputLoader()
-        isLoading = true
         const code = (document.getElementById('input-area') as HTMLTextAreaElement)!.value
         out.innerHTML = code
+        isLoading = true
         isWorking = false
     } else console.log('first')
 }
 update?.addEventListener('click', handleUpdateCode)
+
+/*  ::: Edit Code Button  */
+//////////////////////////////////////////
+const handleEditCode = (event: Event) => {
+    if (isWorking === false) {
+        isWorking = true
+        event.preventDefault()
+        editActive?.classList.toggle('none')
+        updateActive?.classList.toggle('none')
+        display!.style.flex = '1'
+        editor!.style.flex = '4'
+        input?.focus()
+        isWorking = false
+    } else console.log('second')
+}
+edit?.addEventListener('click', handleEditCode)
