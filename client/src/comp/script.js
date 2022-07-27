5fb8c9a7191a6099d29db3a294f4a7b993fd006c
jake-the-cake <askjake331@gmail.com>
2022-07-27 10:58:49 -0400

    select currentcontent started

--- client/src/comp/script.js
+++ client/src/comp/script.js
@@ -8,6 +8,19 @@ const edit = document.getElementById('edit-code');
 const display = document.getElementById('display');
 const editor = document.getElementById('editor');
 const input = document.getElementById('input-area');
+const selector = document.getElementById('selector');
+const htmlTab = document.getElementById('html-tab');
+const cssTab = document.getElementById('css-tab');
+const jsTab = document.getElementById('js-tab');
+let htmlContent = '';
+let cssContent = '';
+let jsContent = '';
+let currentContent;
+const tabs = [
+    [htmlTab, htmlContent],
+    [cssTab, cssContent],
+    [jsTab, jsContent]
+];
 let isLoading = true;
 let isWorking = false;
 /*  ::: Loading widget  */
@@ -34,7 +47,7 @@ const handleUpdateCode = (event) => {
         updateActive === null || updateActive === void 0 ? void 0 : updateActive.classList.toggle('none');
         loader === null || loader === void 0 ? void 0 : loader.classList.toggle('none');
         toggleOutputLoader();
-        const code = document.getElementById('input-area').value;
+        const code = input.value;
         out.innerHTML = code;
         isLoading = true;
         isWorking = false;
@@ -60,3 +73,62 @@ const handleEditCode = (event) => {
         console.log('second');
 };
 edit === null || edit === void 0 ? void 0 : edit.addEventListener('click', handleEditCode);
+/*  ::: Functionality of switching tabs */
+//////////////////////////////////////////
+const handleTabSwitch = (event) => {
+    let wasActive;
+    const changeActiveTab = (active) => {
+        const inactive = tabs.filter(tab => tab[0] !== active);
+        inactive.forEach((t, i) => {
+            var _a, _b;
+            if ((_a = t[0]) === null || _a === void 0 ? void 0 : _a.classList.contains('active-tab')) {
+                (_b = inactive[i][0]) === null || _b === void 0 ? void 0 : _b.classList.remove('active-tab');
+                wasActive = inactive[i][0];
+            }
+        });
+        active === null || active === void 0 ? void 0 : active.classList.add('active-tab');
+    };
+    const changeActiveContent = (active, idx) => {
+        const inactive = tabs.filter(tab => tab[0] !== active);
+        inactive.forEach((t, i) => {
+            if (wasActive === t[0]) {
+                tabs[idx][1] = input.value;
+            }
+        });
+    };
+    switch (event.target) {
+        case htmlTab:
+            changeActiveTab(htmlTab);
+            changeActiveContent(htmlTab, 0);
+            currentContent = tabs[0][1];
+            // currentContent = htmlContent
+            break;
+        case cssTab:
+            changeActiveTab(cssTab);
+            changeActiveContent(cssTab, 1);
+            // if (wasActive === htmlTab) {
+            //     htmlContent = input.value
+            // }
+            // else if (wasActive === jsTab) {
+            //     jsContent = input.value
+            // }
+            currentContent = tabs[1][1];
+            break;
+        case jsTab:
+            changeActiveTab(jsTab);
+            changeActiveContent(jsTab, 2);
+            // if (wasActive === htmlTab) {
+            //     htmlContent = input.value
+            // }
+            // else if (wasActive === cssTab) {
+            //     cssContent = input.value
+            // }
+            currentContent = tabs[2][1];
+            // currentContent = jsContent
+            break;
+        default:
+            break;
+    }
+    input.value = currentContent;
+};
+selector === null || selector === void 0 ? void 0 : selector.addEventListener('click', handleTabSwitch);
--- client/src/raw/script.ts
+++ client/src/raw/script.ts
@@ -8,7 +8,23 @@ const editActive = document.getElementById('control-edit')
 const edit = document.getElementById('edit-code')
 const display = document.getElementById('display')
 const editor = document.getElementById('editor')
-const input = document.getElementById('input-area')
+const input = document.getElementById('input-area') as HTMLTextAreaElement
+const selector = document.getElementById('selector')
+const htmlTab = document.getElementById('html-tab')
+const cssTab = document.getElementById('css-tab')
+const jsTab = document.getElementById('js-tab')
+
+
+let htmlContent = ''
+let cssContent = ''
+let jsContent = ''
+let currentContent: string | HTMLElement | null
+
+const tabs = [
+    [htmlTab, htmlContent],
+    [cssTab, cssContent],
+    [jsTab, jsContent]
+]
 
 let isLoading = true
 let isWorking = false
@@ -37,7 +53,7 @@ const handleUpdateCode = (event: Event) => {
         updateActive?.classList.toggle('none')
         loader?.classList.toggle('none')
         toggleOutputLoader()
-        const code = (document.getElementById('input-area') as HTMLTextAreaElement)!.value
+        const code = input!.value
         out.innerHTML = code
         isLoading = true
         isWorking = false
@@ -60,3 +76,62 @@ const handleEditCode = (event: Event) => {
     } else console.log('second')
 }
 edit?.addEventListener('click', handleEditCode)
+
+/*  ::: Functionality of switching tabs */
+//////////////////////////////////////////
+const handleTabSwitch = (event: Event) => {
+    let wasActive: HTMLElement | null
+    const changeActiveTab = (active: HTMLElement | null) => {
+        const inactive: any[] = tabs.filter(tab => tab[0] !== active)
+        inactive.forEach((t: any[], i) => {
+            if (t[0]?.classList.contains('active-tab')) {
+                inactive[i][0]?.classList.remove('active-tab')
+                wasActive = inactive[i][0]
+            }
+        })
+        active?.classList.add('active-tab')
+    }
+    const changeActiveContent = (active: HTMLElement | null, idx: number) => {
+        const inactive = tabs.filter(tab => tab[0] !== active)
+        inactive.forEach((t,i) => {
+            if (wasActive === t[0]) {
+                tabs[idx][1] = input.value
+            }
+        })
+    }
+    switch (event.target) {
+        case htmlTab:
+            changeActiveTab(htmlTab)
+            changeActiveContent(htmlTab, 0)
+            currentContent = tabs[0][1]
+            // currentContent = htmlContent
+            break
+        case cssTab:
+            changeActiveTab(cssTab)
+            changeActiveContent(cssTab, 1)
+            // if (wasActive === htmlTab) {
+            //     htmlContent = input.value
+            // }
+            // else if (wasActive === jsTab) {
+            //     jsContent = input.value
+            // }
+            currentContent = tabs[1][1]
+            break
+        case jsTab:
+            changeActiveTab(jsTab)
+            changeActiveContent(jsTab, 2)
+            // if (wasActive === htmlTab) {
+            //     htmlContent = input.value
+            // }
+            // else if (wasActive === cssTab) {
+            //     cssContent = input.value
+            // }
+            currentContent = tabs[2][1]
+            // currentContent = jsContent
+            break
+        default:
+            break
+    }
+    input.value = currentContent as string
+}
+selector?.addEventListener('click', handleTabSwitch)
